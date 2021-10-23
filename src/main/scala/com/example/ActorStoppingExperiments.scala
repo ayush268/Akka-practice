7bd133786ae69921af4edf3abb5e726fc148aa0a
Ayush Bansal <ayushb268@gmail.com>
2021-10-24 05:23:05 +0530

    Initial actors + Start of IoT

---
+++ src/main/scala/com/example/ActorStoppingExperiments.scala
@@ -0,0 +1,57 @@
+package com.example
+
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.Signal
+import akka.actor.typed.PostStop
+import akka.actor.typed.ActorSystem
+
+object StartStopActor1 {
+  def apply(): Behavior[String] =
+    Behaviors.setup(new StartStopActor1(_))
+}
+
+class StartStopActor1(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  println("first started")
+  context.spawn(StartStopActor2(), "second")
+
+  override def onMessage(msg: String): Behavior[String] =
+    msg match {
+      case "stop" => Behaviors.stopped
+    }
+
+  override def onSignal: PartialFunction[Signal, Behavior[String]] = {
+    case PostStop =>
+      println("first stopped")
+      this
+  }
+
+}
+
+object StartStopActor2 {
+  def apply(): Behavior[String] =
+    Behaviors.setup(new StartStopActor2(_))
+}
+
+class StartStopActor2(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  println("second started")
+
+  override def onMessage(msg: String): Behavior[String] = {
+    // no messages handled by this actor
+    Behaviors.unhandled
+  }
+
+  override def onSignal: PartialFunction[Signal, Behavior[String]] = {
+    case PostStop =>
+      println("second stopped")
+      this
+  }
+
+}
+
+object ActorStoppingExperiments extends App {
+	val testStartStop = ActorSystem(StartStopActor1(), "testStartStop")
+	testStartStop ! "stop"
+}
